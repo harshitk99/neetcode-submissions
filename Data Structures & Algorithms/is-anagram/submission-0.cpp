108b02704079f66ccb0042b26332984141126fb2
harshitk99 <91711570+harshitk99@users.noreply.github.com>
2026-05-20 12:33:22 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+    
+        return !s.compare(t);
+    }
+};
